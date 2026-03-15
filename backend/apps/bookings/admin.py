9a8b9a0d806c61b50c78990f4ff24fceabde3b53
VenkyRock143 <venky.balusani1@gmail.com>
2026-03-15 19:57:34 +0530

    register all models in Django admin

--- backend/apps/bookings/admin.py
+++ backend/apps/bookings/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin 
+from .models import Booking
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'session', 'status', 
+    'amount_paid', 'booked_at')
+    list_filter = ('status',)
+    search_fields = ('user__email', 'session__title')
--- backend/apps/sessions_app/admin.py
+++ backend/apps/sessions_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin 
+from .models import Category, Session 
+@admin.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'creator', 'status', 
+    'scheduled_at', 'price')
+    list_filter = ('status', 'category') 
+    list_editable = ('status',)
+    search_fields = ('title', 'creator__email') 
+    prepopulated_fields = {'slug': ('title',)} 
+    admin.site.register(Category)
